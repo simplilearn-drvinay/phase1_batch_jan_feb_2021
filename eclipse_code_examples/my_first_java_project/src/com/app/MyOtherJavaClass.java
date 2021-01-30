05ee92a4aafe793398e28df769835b313aedb7a2
vinayingalahalli <vinay.ingalahalli1989@gmail.com>
2021-01-30 16:15:46 +0000

    first java example from eclipse

---
+++ eclipse_code_examples/my_first_java_project/src/com/app/MyOtherJavaClass.java
@@ -0,0 +1,11 @@
+package com.app;
+
+public class MyOtherJavaClass {
+
+	public static void main(String[] args) {
+		System.out.println("Hey Hi again");
+		System.out.print("Hi from print");
+		System.out.println("Hey from println");
+		System.out.println("Hey from println again");
+	}
+}
---
+++ eclipse_code_examples/my_first_java_project/src/mypack/MyFirstJavaClass.java
@@ -0,0 +1,9 @@
+package mypack;
+
+public class MyFirstJavaClass {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World.....");
+	}
+
+}
